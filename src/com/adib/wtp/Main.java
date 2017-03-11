b1163d0ddf7c31c7dc04d0153ab98c917c9e6bba
Adi Bojani <ba_ctm@yahoo.com>
2017-03-11 18:09:00 +0200

    integrate show people

--- src/com/adib/wtp/Main.java
+++ src/com/adib/wtp/Main.java
@@ -1,14 +1,21 @@
 package com.adib.wtp;
 
+import com.adib.wtp.view.PeopleView;
+
 /**
  * Created by adrian.bojani on 3/11/2017.
  */
 public class Main {
-    public void showPeoples(){
+    public static void showPeoples(){
         System.out.println("show People");
+        PeopleView peopleView=new PeopleView();
+        peopleView.setTitle("People view");
+        peopleView.setSize(400,400);
+        peopleView.show();
     }
     public  static void main(String[] args){
         System.out.println("Hello world");
+        showPeoples();
     }
 
 }
