27766f1b7835dca3bd9440fadb0838b245ee23c4
Adi Bojani <ba_ctm@yahoo.com>
2017-03-11 17:43:41 +0200

    Milestone 001

---
+++ src/com/adib/wtp/Main.java
@@ -0,0 +1,11 @@
+package com.adib.wtp;
+
+/**
+ * Created by adrian.bojani on 3/11/2017.
+ */
+public class Main {
+    public  static void main(String[] args){
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/com/adib/wtp/People.java
@@ -0,0 +1,28 @@
+package com.adib.wtp;
+
+/**
+ * Created by adrian.bojani on 3/11/2017.
+ */
+public class People {
+    private String name;
+    private int age;
+
+    public People() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
